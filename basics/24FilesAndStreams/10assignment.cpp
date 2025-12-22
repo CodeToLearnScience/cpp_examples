350746e941c09746d9661a76f24ddd25e2bf4773
CodeToLearnScience <CodeToLearnScience@users.noreply.github.com>
2025-12-22 21:25:02 +0530

    isstringstream example

---
+++ basics/24FilesAndStreams/10assignment.cpp
@@ -0,0 +1,38 @@
+// reading from languages.txt to struct and printing
+// uses istringstream
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+struct language {
+  std::string lang;
+  std::string designer;
+  int date;
+};
+
+int main() {
+
+  std::vector<language> languages;
+  std::ifstream ifile("languages.txt");
+  language lang;
+  std::string line;
+
+  if (!ifile) {
+    std::cout << "Couldn't open the file to read\n";
+    return -1;
+  }
+  while (std::getline(ifile, line)) {
+    std::istringstream is(line);
+    language lang;
+    is >> lang.lang;
+    is >> lang.designer;
+    is >> lang.date;
+    languages.push_back(lang);
+  }
+
+  for (auto &el : languages) {
+    std::cout << el.lang << " " << el.designer << " " << el.date << "\n";
+  }
+}
